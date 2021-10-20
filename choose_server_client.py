dc72792d8ebd5840f6fe09a993307816c56ecd5c
Etienne Sharpin <etienne.sharpin@epita.fr>
2021-10-20 20:32:50 +0200

    FIX: REPL read from stdin

--- choose_server_client.py
+++ choose_server_client.py
@@ -5,7 +5,8 @@ def main():
     n_clients = int(input("Number of clients: "))
     n_servers = int(input("Number of servers: "))
 
-    os.system(f"mpiexec -n {n_clients + n_servers + 1} python3 main.py {n_clients} {n_servers}")
+    n_nodes = n_clients + n_servers
+    os.system(f"mpiexec --stdin {n_nodes} -n {n_nodes + 1} python3 main.py {n_clients} {n_servers}")
 
 
 if __name__ == "__main__":
