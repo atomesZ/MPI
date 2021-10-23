62481468f09075c9831ae6fad1c0d04c42f461df
Etienne Sharpin <etienne.sharpin@epita.fr>
2021-10-23 18:41:40 +0200

    FIX: We are not limited to the number of physical cores for the number of MPI processes

--- choose_server_client.py
+++ choose_server_client.py
@@ -5,8 +5,9 @@ def main():
     n_clients = int(input("Number of clients: "))
     n_servers = int(input("Number of servers: "))
 
-    n_nodes = n_clients + n_servers
-    os.system(f"mpiexec --stdin {n_nodes} -n {n_nodes + 1} python3 main.py {n_clients} {n_servers}")
+    n_nodes = n_clients + n_servers + 1
+
+    os.system(f"mpiexec --host localhost:{n_nodes} --stdin {n_nodes - 1} -n {n_nodes} python3 main.py {n_clients} {n_servers}")
 
 
 if __name__ == "__main__":
