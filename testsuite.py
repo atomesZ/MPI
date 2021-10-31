bc5f017004e63ed56f215e92529d98d2bfe260f1
Etienne Sharpin <etienne.sharpin@epita.fr>
2021-10-31 11:41:19 +0100

    ADD: testsuite, check that all clients commands are present in the log files

--- testsuite.py
+++ testsuite.py
@@ -3,10 +3,12 @@ import os
 import re
 
 r = re.compile("log_server_\d*\.txt")
+r_client = re.compile("client_input_\d*\.txt")
 
-log_files = os.listdir()
+files = os.listdir()
 
-log_files = list(filter(r.match, log_files))
+log_files = list(filter(r.match, files))
+client_input_files = list(filter(r_client.match, files))
 
 errors = []
 
@@ -14,24 +16,43 @@ if len(log_files) == 0:
     errors = ["There are no logfiles saved.\n"]
 else:
     with open(log_files[0]) as f:
-
         f_text = f.readlines()
         for i in range(1, len(log_files)):
             with open(log_files[i]) as f2:
 
                 f2_text = f2.readlines()
-
                 for line in difflib.unified_diff(f_text, f2_text, fromfile=log_files[0], tofile=log_files[i]):
                     errors.append(line)
+
+    # Check that all client_inputs are in the log_files
+
+    errors_client = []
+    for client_file in client_input_files:
+        errors_per_cli = []
+        with open(client_file) as f_client:
+            f_client_text = f_client.readlines()
+            for f_ct in f_client_text:
+                if f_ct not in f_text:
+                    errors_per_cli.append(f_ct)
+        if errors_per_cli:
+            errors_client.append((client_file, errors_per_cli))
 
 GREEN = "\033[92m"
 RED = "\033[91m"
 NC = "\033[0m"
 
-if not errors:
+if not errors and not errors_client:
     print(f"\nTESTSUITE: {GREEN}OK{NC}")
 else:
+    if errors:
+        print(f"\nSERVER LOGS ERRORS :")
     for e in errors:
         print(e, end="")
-    print(f"\nTESTSUITE: {RED}KO{NC}")
+    if errors_client:
+        print(f"\nCLIENT ERRORS :")
+    for e in errors_client:
+        print("\n" + e[0] + ": ")
+        for err_text in e[1]:
+            print(err_text[:-2] + " ", end="")
+    print(f"\n\nTESTSUITE: {RED}KO{NC}")
     exit(1)
